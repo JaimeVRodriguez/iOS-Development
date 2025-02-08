ee5816cf0502e2e29af0311da525b854b2c939f9
jaime <jaimerod26@gmail.com>
2025-02-08 12:42:19 -0500

    update next lesson - state

--- Learn/Learn/ContentView.swift
+++ Learn/Learn/ContentView.swift
@@ -8,14 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isOn: Bool = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            Toggle(isOn: $isOn, label: {
+                Text(!isOn ? "OFF": "ON")
+                    .foregroundStyle(.white)
+            }).fixedSize()
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(isOn ? .yellow : .black)
     }
 }
 
