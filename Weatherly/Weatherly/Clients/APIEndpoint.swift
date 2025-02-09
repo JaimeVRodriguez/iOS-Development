1ba6c1015e16d5afd3af178d68ee02b791b08428
jaime <jaimerod26@gmail.com>
2025-02-08 20:28:35 -0500

    implement API

---
+++ Weatherly/Weatherly/Clients/APIEndpoint.swift
@@ -0,0 +1,29 @@
+//
+//  APIEndpoint.swift
+//  Weatherly
+//
+//  Created by Jaime on 2/8/25.
+//
+
+import Foundation
+
+enum APIEndpoint {
+    static let baseURL = "https://api.openweathermap.org"
+    
+    case coordinatesByLocation(String)
+    case weatherByLatLon(Double, Double)
+    
+    private var path: String {
+        switch self {
+        case .coordinatesByLocation(let city):
+            return "/geo/1.0/direct?q\(city)&appid=\(Constants.Keys.weatherApiKey)"
+        case .weatherByLatLon(let lat, let lon):
+            return "/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(Constants.Keys.weatherApiKey)"
+        }
+    }
+    
+    static func endpointURL(for endpoint: APIEndpoint) -> URL {
+        let endpointPath = endpoint.path
+        return URL(string: baseURL + endpointPath)!
+    }
+}
---
+++ Weatherly/Weatherly/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Weatherly
+//
+//  Created by Jaime on 2/8/25.
+//
+
+import Foundation
+
+struct Constants {
+    struct Keys {
+        static let weatherApiKey = "17700d583970b0dd3189698c83fa3d4a"
+    }
+}
